d877d61232101669e435695ead18454fa318b8b9
mosimchah <mosimchah@gmail.com>
2026-04-08 12:12:41 -0400

    shusky: use PixelParts
    Change-Id: Id2264a197889334941fad43822acec4046012a3d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,3 +27,14 @@ include $(DEVICE_PATH)/wifi/BoardConfig-wifi.mk
 
 # Reserve Space
 include vendor/aicp/config/BoardConfigReservedSize.mk
+
+# PixelParts app
+PRODUCT_PACKAGES += \
+    PixelParts
+
+# PixelParts init rc
+PRODUCT_PACKAGES += \
+    init.pixelparts.rc
+
+# PixelParts sepolicy
+BOARD_SEPOLICY_DIRS += packages/apps/PixelParts/sepolicy
