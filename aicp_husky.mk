b80daf6b4d884f00c096c20394a427a0ce4cfd0a
mosimchah <mosimchah@gmail.com>
2026-04-08 12:12:41 -0400

    shusky: use PixelParts
    Change-Id: Id2264a197889334941fad43822acec4046012a3d

--- aicp_husky.mk
+++ aicp_husky.mk
@@ -32,3 +32,14 @@ $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
+
+# PixelParts app
+PRODUCT_PACKAGES += \
+    PixelParts
+
+# PixelParts init rc
+PRODUCT_PACKAGES += \
+    init.pixelparts.rc
+
+# PixelParts sepolicy
+BOARD_SEPOLICY_DIRS += packages/apps/PixelParts/sepolicy
